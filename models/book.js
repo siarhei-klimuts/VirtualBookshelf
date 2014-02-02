f8f09392191588a19f49b61e3b1a24aeb3b2665b
Siarhei Klimuts <galiaf47@gmail.com>
2014-02-02 22:18:51 +0300

    heroku troubles

--- models/book.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('book', {
-		pos_x: DataTypes.FLOAT,
-		pos_y: DataTypes.FLOAT,
-		pos_z: DataTypes.FLOAT,
-		author: DataTypes.STRING,
-		title: DataTypes.STRING
-	}, {timestamps: false});
-} 
--- models/libraryObject.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('libraryObject', {
-		model: DataTypes.STRING
-	}, {timestamps: false});
-} 
