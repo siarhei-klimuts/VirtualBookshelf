17654093a14a546020d27e228a4ed93683e72cf3
Siarhei Klimuts <galiaf47@gmail.com>
2014-03-02 20:36:14 +0300

    Try to update sequelize to 1.7.0

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,10 @@
-var request = require('request');
-
 exports.index = function(req, res){
 	var user = req.user;
   	res.render('index', {user: user});
 };
 
 exports.getOutside = function(req, res) {
-	var link = req.query.link;
-	try {
-		request(link).pipe(res);
-	} catch(exception) {
-		res.send(304);
-		console.log('catch link',exception);
-	}
+	res.redirect(req.query.link);
 }
 
 exports.library = require('./library');
