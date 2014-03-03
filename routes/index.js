b78864f7d81e035dffd1554967e1650c4bee0f01
Siarhei Klimuts <galiaf47@gmail.com>
2014-03-03 23:52:37 +0300

    Returned request module for proxy

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
+var request = require('request');
 exports.index = function(req, res){
 	var user = req.user;
   	res.render('index', {user: user});
 };
 
 exports.getOutside = function(req, res) {
-	res.redirect(req.query.link);
+	// res.proxy(req.query.link);
+	//**
+	try {
+		console.log('try link',req.query.link);
+		request(req.query.link).pipe(res);
+	} catch(exception) {
+		res.send(404);
+		console.log('catch link',exception);
+	}
+  //****
 }
 
 exports.library = require('./library');
