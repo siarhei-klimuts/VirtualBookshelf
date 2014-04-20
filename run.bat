301efe0abe699bbe7388a99bb05cae2c958a4b59
Siarhei Klimuts <Galiaf47@users.noreply.github.com>
2014-04-20 21:43:38 +0300

    Delete run.bat

--- run.bat
+++
@@ -1,3 +0,0 @@
-set NODE_HOST=http://127.0.0.1:3000
-set DATABASE_URL=postgres://atdmajapxbpbpz:yQy6mVgEJUrl105JJzWYOu1O2R@ec2-54-228-227-68.eu-west-1.compute.amazonaws.com:5432/da3tfangtunmh6?ssl=true
-nodemon --ignore public/ --ignore views/ app.js
