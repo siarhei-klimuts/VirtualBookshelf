5cb46600197a8c1aadb29c7711fa76127dcc84a7
Siarhei Klimuts <galiaf47@gmail.com>
2016-04-07 20:35:10 +0300

    Disable dotenv for prod

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-require('dotenv').config({silent: true});
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({silent: true});
+}
 
 var express = require('express');
 var routes = require('./routes');
